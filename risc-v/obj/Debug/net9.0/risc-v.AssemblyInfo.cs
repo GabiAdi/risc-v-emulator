20205019f1a4ca42a3f7cf3bc065e1efc092a071
GabiAdi <gabi3adi@gmail.com>
2026-05-12 16:53:32 +0200

    Push bios for systems that cannot assemble it

--- risc-v/obj/Debug/net9.0/risc-v.AssemblyInfo.cs
+++ risc-v/obj/Debug/net9.0/risc-v.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("risc-v")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b35f83e2f058d4163accca76fc659b5cfbaa09f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b77164c3246c92a32e824b4bdc70b17dd2b405")]
 [assembly: System.Reflection.AssemblyProductAttribute("risc-v")]
 [assembly: System.Reflection.AssemblyTitleAttribute("risc-v")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
